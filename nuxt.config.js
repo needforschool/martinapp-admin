de9413bb51e661a1e5a88a2e1f924b2efb36d5a0
ImJustLucas <bellierlucas.pro@gmail.com>
2022-06-02 16:22:33 +0200

    💄 edit global scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,27 +3,31 @@ export default {
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Admin',
+    title: "Admin",
     htmlAttrs: {
-      lang: 'fr',
+      lang: "fr",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap",
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/scss/main.scss',
-  ],
+  css: ["~/assets/scss/main.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -32,21 +36,18 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    "@nuxtjs/dotenv",
-    '@nuxtjs/style-resources',
-  ],
+  buildModules: ["@nuxtjs/dotenv", "@nuxtjs/style-resources"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: "/",
   },
 
   styleResources: {
---
+++ pages/login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="login">
+    <div class="login--card card">
+      <h2>Connexion</h2>
+      <form @submit.prevent="login">
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            v-model="email"
+            placeholder="Email"
+          />
+        </div>
+        <div class="form-group">
+          <label for="password">Mot de passe</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            v-model="password"
+            placeholder="Mot de passe"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">Connexion</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    }
+  },
+  methods: {
+    login() {
+      console.log(this.email, this.password)
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+
+  &--card {
+    width: 90%;
+    height: 600px;
+
+    @include min-lg {
+      width: 40%;
+      height: 700px;
+    }
+  }
+}
+</style>
